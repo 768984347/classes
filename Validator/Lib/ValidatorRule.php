9e753730973429ddea51a4138d7f2676defdc3be
mxc <mxc@itomix.com.cn>
2018-07-18 11:17:52 +0800

    优化验证类validator

--- Validator/Lib/ValidatorRule.php
+++ Validator/Lib/ValidatorRule.php
@@ -99,49 +99,122 @@ class ValidatorRule
      */
     public function file($key)
     {
-        if (is_array($_FILES[$key]['tmp_name'])) {
-            foreach ($_FILES[$key]['tmp_name'] as $no => $tmp_name) {
-                if (! is_uploaded_file($tmp_name) || $_FILES[$key][$no] !== 0)
-                    return false;
-            }
-        } else {
-            return is_uploaded_file($_FILES[$key]['tmp_name']) && $_FILES[$key]['error'] === 0;
-        }
-        return true;
-    }
-
-    protected function jpg($mime)
-    {
-        return $mime === 'image/jpeg';
-    }
-
-    public function extension($key, $extension_array)
-    {
-        $class_method = get_class_methods(get_class());
-        $extension_array = explode($this->getExplodeMethodParamSign(), $extension_array);
-        $mime = null;
-        if ($this->file($key)) {
-            if (is_array($_FILES[$key]['type']))
-                $mime = $_FILES[$key]['type'];
-            else
-                $mime = [$_FILES[$key]['type']];
-        } elseif (is_file($this->data[$key])) {
-            $mime = [$this];
-        }
-        foreach ($extension_array as $extension) {
-            if (in_array($extension, $class_method) && $mime) {
-                foreach ($mime as $file_mime) {
-                    if (! call_user_func([$this, $extension], $file_mime))
-                        break;
+        if (isset($_FILES[$key])) {
+            if (is_array($_FILES[$key]['tmp_name'])) {
+                foreach ($_FILES[$key]['tmp_name'] as $no => $tmp_name) {
+                    if (! is_uploaded_file($tmp_name) || $_FILES[$key][$no] !== 0)
+                        return false;
                 }
                 return true;
+            } else {
+                return is_uploaded_file($_FILES[$key]['tmp_name']) && $_FILES[$key]['error'] === 0;
             }
         }
         return false;
     }
 
+    /**
+     * 验证jpg
+     * @param $mime
+     * @return bool
+     */
+    protected function jpg($mime)
+    {
+        return $mime === 'image/jpeg';
+    }
+
+    /**
+     * 验证png
+     * @param $mime
+     * @return bool
+     */
+    protected function png($mime)
+    {
+        return $mime === 'image/png';
+    }
+
+    /**
+     * 普通文件使用的验证方法
+     * @param $key
+     * @param $extension_str
+     * @return bool
+     */
+    public function extension($key, $extension_str)
+    {
+        $extension_array = $this->getMethodParam($extension_str);
+        $mime = [];
+        if (is_string($this->data[$key])) {
+            if (is_file($this->data[$key])) {
+                $mime = [filetype($this->data[$key])];
+            } else {
+                $mime = [pathinfo($this->data[$key], PATHINFO_EXTENSION)];
+            }
+        }
+        if (! empty($mime))
+            return $this->checkFileType($mime, $extension_array);
+        return false;
+    }
+
+    /**
+     * $_FILES文件上传使用的验证方法
+     * @param $key
+     * @param $extension_str
+     * @return bool
+     */
+    public function fileExtension($key, $extension_str)
+    {
+        $extension_array = $this->getMethodParam($extension_str);
+        if ($this->file($key)) {
+            $mime = [];
+            //如果是多文件上传
+            if (is_array($_FILES[$key]['type'])) {
+                $mime = $_FILES[$key]['type'];
+            } else {
+                $mime = [$_FILES[$key]['type']];
+            }
+            return $this->checkFileType($mime, $extension_array);
+        }
+        return false;
+    }
+
+    protected function checkFileType(array $mime, array $extension_array)
+    {
+        $class_method = get_class_methods(get_class());
+        //只要有一个extension命中就通过
+        foreach ($extension_array as $extension) {
+            //如果是一个方法
+            if (in_array($extension, $class_method)) {
+                foreach ($mime as $file_mime) {
+                    //只要有一个mime没有通过就不通过
+                    if (! call_user_func([$this, $extension], $file_mime))
+                        break;
+                    return true; //只要有一个extension命中就通过
+                }
+            } else {
+                //命中自定义的extension就算通过
+                if (in_array($extension, $mime))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 获取给方法传参的分割符号
+     * @return string
+     */
     protected function getExplodeMethodParamSign()
     {
         return ',';
+    }
+
+    /**
+     * 通过分隔符号获取数组
+     * @param $param_str
+     * @return array
+     */
+    protected function getMethodParam($param_str)
+    {
+        return explode($this->getExplodeMethodParamSign(), $param_str);
     }
 }
